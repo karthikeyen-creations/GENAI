6c302bbb39bd03f33b3ca432e5021c81b9a7c267
karthikeyen-creations <55886253+karthikeyen-creations@users.noreply.github.com>
2024-10-24 12:15:47 +0530

    Update streamlit_historic_data_query.py

--- Stock Sentiment Analysis/streamlit_historic_data_query.py
+++ Stock Sentiment Analysis/streamlit_historic_data_query.py
@@ -27,6 +27,8 @@ from py.db_storage import *
 
 load_dotenv()
 
+cmp_tr=""
+
 azure_config = {
         "base_url": os.getenv("AZURE_OPENAI_ENDPOINT"),
         "embedding_base_url": os.getenv("AZURE_OPENAI_EMBEDDING_ENDPOINT"),
@@ -87,7 +89,6 @@ Note: For Demo purpose, historical data is available only for the below companie
 </h4>""", 
 unsafe_allow_html=True)
 
-
 def create_models(azure_config):
     print("creating models")
     llm = AzureChatOpenAI(temperature=0,
@@ -106,16 +107,14 @@ def create_models(azure_config):
     models.embedding_model=embedding_model 
     return models
 
-def user_input(user_question):
-    print("Question: ", user_question)
-    
+def get_ticker(user_question):
     
     qna_system_message = """
     You are an assistant to a financial services firm who finds the 'nasdaq company ticker' of the company in the question provided.
 
     User questions will begin with the token: ###Question.
 
-    Please find the 'nasdaq company ticker' of the company in the question provided.
+    Please find the 'nasdaq company ticker' of the company in the question provided. In case of an invalid NASDAQ company, return "NOTICKER".
     
     Response format:
     {nasdaq company ticker}
@@ -151,15 +150,116 @@ def user_input(user_question):
             temperature=0
         )
 
-        cmp_tr = response.choices[0].message.content.strip()
+        cmp_tkr = response.choices[0].message.content.strip()
     except Exception as e:
-        cmp_tr = f'Sorry, I encountered the following error: \n {e}'
-        st.write("Reply: ", cmp_tr)
+        cmp_tkr = f'Sorry, I encountered the following error: \n {e}'
+        st.write("Reply: ", cmp_tkr)
         return
+    print(cmp_tkr)
+    return(cmp_tkr)
+    
+    
+def user_input(user_question):
+    print("Question: ", user_question)
+    
+    cmp_tr=get_ticker(user_question)
+    
+    # Initialise ChromaDB Database
+    chroma_db = DBStorage()
+    chroma_db.load_vectors()
+    context_for_query = chroma_db.get_context_for_query(cmp_tr,k=5)
+    
+    
+    qna_system_message = """
+    You are an assistant to a financial services firm who answers user queries on Stock Investments.
+    User input will have the context required by you to answer user questions.
+    This context will begin with the token: ###Context.
+    The context contains references to specific portions of a document relevant to the user query.
+
+    User questions will begin with the token: ###Question.
+    
+    First, find the 'nasdaq company ticker' of the related company in the question provided.
+    Your task is to perform sentiment analysis on the content part of each documents provided in the Context, which discuss a company identified by its 'nasdaq company ticker'. The goal is to determine the overall sentiment expressed across all documents and provide an overall justification. Based on the sentiment analysis, give a recommendation on whether the company’s stock should be purchased.
+
+    Step-by-Step Instructions:
+        1. Read the Context: Carefully read the content parts of each document provided in the list of Documents.
+        2. Determine Overall Sentiment: Analyze the sentiment across all documents and categorize the overall sentiment as Positive, Negative, or Neutral.
+        3. Provide Overall Justification: Summarize the key points from all documents to justify the overall sentiment.
+        4. Stock Advice: Based on the overall sentiment and justification, provide a recommendation on whether the company’s stock should be purchased.
+
+    Example Analysis:
+        Context: 
+            [Document(metadata={'platform': 'Google News', 'company': 'GOOGL', 'ingestion_timestamp': '2024-10-22T10:11:17.257275', 'word_count': 91}, page_content="{'title': 'Alphabet Inc. (GOOGL): Among the Most Owned Stocks by Hedge Funds Right Now - Insider Monkey', 'content': 'Alphabet Inc. (GOOGL): Among the Most Owned Stocks by Hedge Funds Right Now  Insider Monkey'}"),Document(metadata={'platform': 'Google News', 'company': 'GOOGL', 'ingestion_timestamp': '2024-10-22T10:11:17.257275', 'word_count': 59}, page_content="{'title': 'Here\'s Why Alphabet (GOOGL) is a Strong Momentum Stock - MSN', 'content': 'Here\'s Why Alphabet (GOOGL) is a Strong Momentum Stock  MSN'}")]
+    
+    Response Format:
+    If the Question is "NOTICKER":
+        No valid company in the query.
+        
+    If the content parts of context has relevent data:
+        Overall Sentiment: Positive
+        Overall Justification: The Historical documents consistently present Alphabet Inc. (GOOGL) in a positive light. One highlights it as being highly owned by hedge funds, indicating institutional confidence, while the other describes it as a strong momentum stock, pointing to positive market performance expectations.
+        Stock Advice: Based on the positive sentiments expressed in the Historical documents, it is advisable to consider purchasing Alphabet Inc. (GOOGL) stock. The positive perceptions among hedge funds and the stock’s status as a strong momentum stock suggest potential for future success.
+   
+    Else:
+        Respond "Company {Company name}Alphabet Inc.(Google) {nasdaq company ticker}(GOOGL) details not found in the Historical Data".
+    
+    Please follow the steps to analyze the sentiment of each document’s content; and exact structure illustrated in above example response to provide an overall sentiment, justification and give stock purchase advice. Provide only Overall response, don't provide documentwise response or any note. Decorate the reponse with html/css tags.
+    
+    """
+
+    qna_user_message_template = """
+    ###Context
+    Here are some documents that are relevant to the question mentioned below.
+    {context}
+
+    ###Question
+    {question}
+    """
+
+    client = AzureOpenAI(
+    azure_endpoint=azure_config["base_url"],
+    api_key=azure_config["api-key"],
+    api_version="2024-02-01"
+    )
+    
+    prompt = [
+        {'role':'system', 'content': qna_system_message},
+        {'role': 'user', 'content': qna_user_message_template.format(
+            context=context_for_query,
+            # question=user_question
+            question=cmp_tr
+            )
+        }
+    ]
+    
+    try:
+        response = client.chat.completions.create(
+            model=azure_config["model_name"],
+            messages=prompt,
+            temperature=0
+        )
+
+        prediction = response.choices[0].message.content.strip()
+    except Exception as e:
+        prediction = f'Sorry, I encountered the following error: \n {e}'
 
     
-    print(cmp_tr)
+    print(prediction)
+    sentiment=extract_between(prediction,"Overall Sentiment: ","Overall Justification:").strip()
+    if sentiment == "Positive":
+        st.success("Positive : Go Ahead...!")
+    elif sentiment == "Negative":
+        st.warning("Negative : Don't...!")
+    elif sentiment == "Neutral":
+        st.info("Neutral : Need to Analyse further")
+    st.write( prediction, unsafe_allow_html=True)
+    return(cmp_tr)
     
+    
+def real_time(cmp_tr):
+    
+    st.info("IN DEVELOPMENT")
+    print(cmp_tr)
     # Initialise ChromaDB Database
     chroma_db = DBStorage()
     chroma_db.load_vectors()
@@ -190,11 +290,11 @@ def user_input(user_question):
     Response Format:
     If the content parts of context has relevent data:
         Overall Sentiment: Positive
-        Overall Justification: The Historical documents consistently present Alphabet Inc. (GOOGL) in a positive light. One highlights it as being highly owned by hedge funds, indicating institutional confidence, while the other describes it as a strong momentum stock, pointing to positive market performance expectations.
-        Stock Advice: Based on the positive sentiments expressed in the Historical documents, it is advisable to consider purchasing Alphabet Inc. (GOOGL) stock. The positive perceptions among hedge funds and the stock’s status as a strong momentum stock suggest potential for future success.
+        Overall Justification: The RealTime documents consistently present Alphabet Inc. (GOOGL) in a positive light. One highlights it as being highly owned by hedge funds, indicating institutional confidence, while the other describes it as a strong momentum stock, pointing to positive market performance expectations.
+        Stock Advice: Based on the positive sentiments expressed in the RealTime documents, it is advisable to consider purchasing Alphabet Inc. (GOOGL) stock. The positive perceptions among hedge funds and the stock’s status as a strong momentum stock suggest potential for future success.
    
     Else:
-        Respond "Company {Company name}Alphabet Inc.(Google) {nasdaq company ticker}(GOOGL) details not found in the Historical Data".
+        Respond "Company {Company name}Alphabet Inc.(Google) {nasdaq company ticker}(GOOGL) details not found in the RealTime Data".
     
     Please follow the steps to analyze the sentiment of each document’s content; and exact structure illustrated in above example response to provide an overall sentiment, justification and give stock purchase advice. Provide only Overall response, don't provide documentwise response. Decorate the reponse with html/css tags.
     
@@ -219,7 +319,7 @@ def user_input(user_question):
         {'role':'system', 'content': qna_system_message},
         {'role': 'user', 'content': qna_user_message_template.format(
             context=context_for_query,
-            question=user_question
+            question=cmp_tr
             )
         }
     ]
@@ -238,13 +338,13 @@ def user_input(user_question):
     
     print(prediction)
     sentiment=extract_between(prediction,"Overall Sentiment: ","Overall Justification:").strip()
-    if sentiment == "Positive":
-        st.success("Positive : Go Ahead...!")
-    elif sentiment == "Negative":
-        st.warning("Negative : Don't...!")
-    elif sentiment == "Neutral":
-        st.warning("Neutral : Need to Analyse further")
-    st.write( prediction, unsafe_allow_html=True)
+    # if sentiment == "Positive":
+    #     st.success("Positive : Go Ahead...!")
+    # elif sentiment == "Negative":
+    #     st.warning("Negative : Don't...!")
+    # elif sentiment == "Neutral":
+    #     st.info("Neutral : Need to Analyse further")
+    # st.write( prediction, unsafe_allow_html=True)
     
 def extract_between(text: str, start: str, end: str) -> str:
     """
@@ -299,7 +399,7 @@ def main():
         
         with st.container():
             if user_question:
-                user_input(user_question)
+                cmp_tr = user_input(user_question)
 
     # Second column content
     with col2:
@@ -307,7 +407,7 @@ def main():
         
         with st.container():
             if user_question:
-                user_input(user_question)
+                real_time(cmp_tr)
 
     # Add a footer
     st.markdown("---")
